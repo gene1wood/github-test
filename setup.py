ced56b5913e29ff6aab291273a45be0fb2a42110
Gene Wood <gene_wood@cementhorizon.com>
2020-05-01 12:23:16 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='github-actions-build-release-test',
+    version='0.1',
+    license='BSD',
+    author='Gene',
+    author_email='test@example.com',
+    url='http://www.example.com/',
+    packages=['github_action_build_release_test'],
+    description="Test",
+)
